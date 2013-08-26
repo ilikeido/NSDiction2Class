6b483c1688842de88c49b5fa39344df3c657ee61
hesh <hesh@heshtekiMacBook-Pro.local>
2013-08-26 11:35:00 +0800

    update download url

--- NSDictionary+NSDictionary2Object.h
+++ NSDictionary+NSDictionary2Object.h
@@ -4,6 +4,7 @@
 //
 //  Created by hesh on 13-8-22.
 //  Copyright (c) 2013年 ilikeido. All rights reserved.
+//  https://github.com/ilikeido/NSDictionary2Object
 //
 
 #import <Foundation/Foundation.h>
